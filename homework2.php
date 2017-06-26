90df399479315c0ad5f49f1ce2fe14903b57c547
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-06-26 09:02:48 +0300

    homework2 done

--- homework2.php
+++ homework2.php
@@ -2,7 +2,6 @@
   require "homework2functions.php";
   //include "functions.php";
 ?>
-
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,30 +9,25 @@
     </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
-
       th, td {
         text-align: center;
         vertical-align: middle;
       }
       .green {
-        background-color: green;
+        background-color: rgba(11, 133, 0, 0.4);
       }
-
     </style>
   </head>
   <body>
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h1>Kaip jautiesi lauke?</h1>
+          <h1>Baseino užpildymo kaštai</h1>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
           <table class="table table-bordered">
-          
-
-
             <?php 
             $ilgis = 20;
             $plotis = 5;
@@ -41,24 +35,18 @@
             $autocis2 = 400;
             $autocis3 = 1200;
 
-
-
-
             echo "<tr><th rowspan='3'>Baseino gylis (m)</th> <th rowspan='3'> Baseino tūris (m3)</th><th colspan='6'>Autocisternų kiekis (vnt.)</th></tr><tr><th colspan='2'>250 m3</th><th colspan='2'>400 m3</th><th colspan='2'>1200 m3</th></tr><tr><th>Kiekis</th><th>Kaina</th><th>Kiekis</th><th>Kaina</th><th>Kiekis</th><th>Kaina</th></tr>";
+
             for ($gylis=1; $gylis<=30; $gylis++) { 
-              
-              $kain1 = "white";
-              $kain2 = "white";
-              $kain3 = "white";
-
-              spalvinimas($kain1, $kain2, $kain3, kaina($autocis1, autocisternos(turis($ilgis, $plotis, $gylis), $autocis3)), kaina($autocis2, autocisternos(turis($ilgis, $plotis, $gylis), $autocis3)), kaina($autocis3, autocisternos(turis($ilgis, $plotis, $gylis), $autocis3)));
-
-              echo "<tr><td>" . $gylis . "</td><td>" . turis($ilgis, $plotis, $gylis) . "</td><td>" . autocisternos(turis($ilgis, $plotis, $gylis), $autocis1) . "</td><td class=" . $kain1 . ">" . kaina($autocis1, autocisternos(turis($ilgis, $plotis, $gylis), $autocis1)) . "</td><td>" . autocisternos(turis($ilgis, $plotis, $gylis), $autocis2) . "</td><td class=" . $kain2 . ">" . kaina($autocis2, autocisternos(turis($ilgis, $plotis, $gylis), $autocis2)) . "</td><td>" . autocisternos(turis($ilgis, $plotis, $gylis), $autocis3) . "</td><td class=" . $kain3 . ">" . kaina($autocis3, autocisternos(turis($ilgis, $plotis, $gylis), $autocis3)) . "</td></tr>";
-
-              
-
+              $turis =  turis($ilgis, $plotis, $gylis);
+              $autocisterna250 = autocisternos($turis, $autocis1);
+              $kaina250 = kaina($autocis1, $autocisterna250);
+              $autocisterna400 = autocisternos($turis, $autocis2);
+              $kaina400 = kaina($autocis2, $autocisterna400);
+              $autocisterna1200 = autocisternos($turis, $autocis3);
+              $kaina1200 = kaina($autocis3, $autocisterna1200);
+              echo "<tr><td>" . $gylis . "</td><td>" . $turis . "</td><td>" . $autocisterna250 . "</td><td class=" . spalvinimas($kaina250, $kaina400, $kaina1200) . ">" . $kaina250 . "</td><td>" . $autocisterna400 . "</td><td class=" . spalvinimas($kaina400, $kaina250, $kaina1200) . ">" . $kaina400 . "</td><td>" . $autocisterna1200 . "</td><td class=" . spalvinimas($kaina1200, $kaina400, $kaina250) . ">" . $kaina1200 . "</td></tr>";
             }
-           
             ?>
           </table>
         </div>
--- homework2functions.php
+++ homework2functions.php
@@ -3,7 +3,6 @@ function turis($ilgis, $plotis, $gylis) {
 	$turis = $ilgis*$plotis*$gylis;
 	return $turis;
 }
-
 function autocisternos($turis, $autocis) {
 	$autocisternu_kiekis = ceil($turis/$autocis);
 	return $autocisternu_kiekis;
@@ -12,18 +11,11 @@ function kaina($autocis, $autocisternu_kiekis) {
 	$kaina = round($autocisternu_kiekis*($autocis*0.75+50));
 	return $kaina;
 }
-function spalvinimas($klase1, $klase2, $klase3, $kaina1, $kaina2, $kaina3) {
-	if ($kaina1 < $kaina2 && $kaina1 < $kaina3) {
-		$klase1 = "green";
-		return $klase1;
-	} elseif ($kaina2 < $kaina1 && $kaina2 < $kaina3) {
-		$klase2 = "green";
-		return $klase2;
-	} elseif ($kaina3 < $kaina1 && $kaina3 < $kaina2) {
-		$klase3 = "green";
-		return $klase3;
-
+function spalvinimas($kaina1, $kaina2, $kaina3) {
+	if ($kaina1 <= $kaina2 && $kaina1 <= $kaina3) {
+		return "green";
+	} else {
+		return "white";
 	}
 }
-
 ?>
